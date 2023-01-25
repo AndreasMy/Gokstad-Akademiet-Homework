71922d6d72a53bf89827f4b92cf9a2c47ba75e5e
AndreasMy <amykle85@gmail.com>
2023-01-25 18:30:59 +0100

    attempt to solve oppgave2

--- script/oppgave2.js
+++ script/oppgave2.js
@@ -11,9 +11,27 @@ const tenCities = [
   "Tønsberg",
 ];
 
+const firstThree = [];
+const lastFive = [];
+
 const displayTenCities = document.querySelector("#displayTenCities");
+const displayThreeFirst = document.querySelector("#displayThreeCities");
+const displayFiveLast = document.querySelector("#displayFiveCities");
+
 for (i = 0; i < tenCities.length; i++) {
-    displayTenCities.innerHTML += `<li>${tenCities[i]}</li>`;
+  displayTenCities.innerHTML += `<li>${tenCities[i]}</li>`;
+  
+  if (i < 3) {
+      firstThree.splice(3, 0, tenCities[i]);
+      displayThreeFirst.innerHTML += `<li>${firstThree[i]}</li>`;
+  } else if (i > 4) {
+    lastFive.splice(5, 0, tenCities[i]);
+    displayFiveLast.innerHTML += `<li>${lastFive[i]}</li>`;
+  }
 }
 
 // map() for å kopiere til nye arrays?
+
+console.log(tenCities);
+console.log(firstThree);
+console.log(lastFive);
