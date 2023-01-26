e5de78ea8d60ef0d13cb1ca9b478112dbefd158e
AndreasMy <amykle85@gmail.com>
2023-01-26 19:37:49 +0100

    better solution to oppgave2

--- script/oppgave2.js
+++ script/oppgave2.js
@@ -11,16 +11,17 @@ const tenCities = [
   "Tønsberg",
 ];
 
-const firstThree = [];
-const lastFive = [];
+let firstThree = [];
+let lastFive = [];
 
 const displayTenCities = document.querySelector("#displayTenCities");
 const displayThreeFirst = document.querySelector("#displayThreeCities");
 const displayFiveLast = document.querySelector("#displayFiveCities");
 
-for (i = 0; i < tenCities.length; i++) {
+/* for (i = 0; i < tenCities.length; i++) {
   displayTenCities.innerHTML += `<li>${tenCities[i]}</li>`;
   lastFive.splice(0, 0, tenCities[i]);
+  console.log(`add ${lastFive}`);
 
   if (i < 3) {
     firstThree.splice(3, 0, tenCities[i]);
@@ -30,9 +31,21 @@ for (i = 0; i < tenCities.length; i++) {
   if (i > 4) {
     const newArr = lastFive.shift();
     displayFiveLast.innerHTML += `<li>${newArr}</li>`;
+    console.log(`shift ${newArr}`);
   }
 }
 
 console.log(tenCities);
 console.log(firstThree);
-console.log(lastFive);
+console.log(lastFive); */
+
+
+
+for (i = 0; i < tenCities.length; i++) {
+  displayTenCities.innerHTML += `<li>${tenCities[i]}</li>`;
+  if (i < 3) {
+    displayThreeFirst.innerHTML += `<li>${tenCities[i]}</li>`;
+  } else if (i > 4) {
+    displayFiveLast.innerHTML += `<li>${tenCities[i]}</li>`;
+  }
+}
