4e640829fed1f069943fd31a9077298ddffd2827
AndreasMy <amykle85@gmail.com>
2023-02-01 10:35:01 +0100

    navbar is created with a for loop

---
+++ script/navigation.js
@@ -0,0 +1,6 @@
+// make loop that changes inerHTML for the nav bar
+const navBar = document.querySelector("#navLinks");
+
+for (let i = 1; i < 5; i++) {
+  navBar.innerHTML += `<li><a href="../pages/oppgave-${i}.html">Oppgave ${i} </a></li>`;
+}
